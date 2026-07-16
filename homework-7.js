3a940ab8297c609067a6acc22c31b751c41e08b7
ТвойНик <edlargonfrost@gmail.com>
2026-07-16 16:52:18 +0300

    занкомство с джава скрипт и его сущностями

---
+++ homework-7.js
@@ -0,0 +1,29 @@
+let city = 'кувейт'
+let temp = '40' 
+console.log(`сейчас в городе ${city} температура ${temp} градусов`)
+
+
+const speedOfLight = 299792458
+let speed = 299792459
+if (speedOfLight < speed) {
+  console.log('сверхсветовая скорость')
+};
+
+
+let product = "HyperX cloud 2 wireless"
+let price = 15000
+let budget = 20000
+if (budget >= price) {
+  console.log(`${product} приобретён. Спасибо за покупку!`)
+} else {
+  console.log(`Вам не хватает ${price - budget}$, пополните баланс`)
+}
+
+
+let programming = "реально крутая вещь"
+console.log(`javascript это ${programming}`);
+
+
+const lost = 4815162342
+let char = "ghost rider"
+var name = "rerur"
