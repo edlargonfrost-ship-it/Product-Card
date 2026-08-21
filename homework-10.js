8abdc33f5416c1e7f4430a3c6a71e9f52116bb0b
ТвойНик <edlargonfrost@gmail.com>
2026-08-21 05:50:19 +0300

    I did the 2 functions as the task said, there's a problem with input validation, I don't know how to fix it

--- homework-10.js
+++ homework-10.js
@@ -3,14 +3,24 @@ import { cards } from "./cards.js";
 const cardTemplate = document.getElementById("card-template");
 const cardList = document.getElementById("card-list");
 
-const cardsToShow = Number(prompt("Сколько карточек отобразить? От 1 до 5"));
 
-console.log("Ввёл:", cardsToShow);
+function getCardsAmount(){
+  const cardsToShow = Number(prompt("Сколько карточек отобразить? От 1 до 5"));
+
+  if(cardsToShow < 1){return 0}
+    else return cardsToShow
+  if(cardsToShow > 5){return 0}
+  else return cardsToShow
+}
+
+const getCards = getCardsAmount();
+console.log("Итоговое количество:", getCards);
+console.log("Ввёл:", getCards);
 console.log("Всего карточек:", cards.length);
-console.log("Будет показано:", cards.slice(0, cardsToShow).length);
+console.log("Будет показано:", cards.slice(0, getCards).length);
 
-cards.slice(0, cardsToShow).forEach(card => {
-
+function renderCards(){
+  cards.slice(0, getCards).forEach(card => {
   const cardFilling = cardTemplate.content.cloneNode(true);
 
   cardFilling.querySelector(".card__image").src = card.image;
@@ -30,6 +40,9 @@ cards.slice(0, cardsToShow).forEach(card => {
 
   cardList.appendChild(cardFilling);
 });
+};
+
+renderCards()
 
 const cardsArray = cards.reduce((array, card) =>{
   array.push({
